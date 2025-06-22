0bb9da9875aea5d1d29c2370ffe749be6ca641ba
TristanKatte <tristankatte@hotmail.com>
2025-06-22 16:27:43 +0200

    updated rss server

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,29 +1,38 @@
-import * as config from '$lib/config'
-import type { Post } from '$lib/types'
+import * as config from '$lib/config';
+import type { Post } from '$lib/types';
 
-export const prerender = true
+export const prerender = true;
+
+function escapeXml(unsafe: string) {
+	return unsafe
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&apos;');
+}
 
 export async function GET({ fetch }) {
-	const response = await fetch('api/issues')
-	const posts: Post[] = await response.json()
+	const response = await fetch('/api/issues');
+	const posts: Post[] = await response.json();
 
-	const headers = { 'Content-Type': 'application/xml' }
+	const headers = { 'Content-Type': 'application/xml' };
 
 	const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
 			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
+				<title>${escapeXml(config.title)}</title>
+				<description>${escapeXml(config.description)}</description>
 				<link>${config.url}</link>
 				<atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
 				${posts
 					.map(
 						(post) => `
 						<item>
-							<title>${post.title}</title>
-							<description>${post.description}</description>
-							<link>${config.url}/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}/${post.slug}</guid>
+							<title>${escapeXml(post.title)}</title>
+							<description>${escapeXml(post.description)}</description>
+							<link>${config.url}/issues/${post.slug}</link>
+							<guid isPermaLink="true">${config.url}/issues/${post.slug}</guid>
 							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
 						</item>
 					`
@@ -31,7 +40,7 @@ export async function GET({ fetch }) {
 					.join('')}
 			</channel>
 		</rss>
-	`.trim()
+	`.trim();
 
-	return new Response(xml, { headers })
+	return new Response(xml, { headers });
 }
